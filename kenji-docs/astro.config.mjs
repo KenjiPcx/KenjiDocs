e601c638ac342a029a69ce1d7b0907dbfc51aff6
Kenji Phang <ken.pcx@outlook.com>
2023-01-26 04:17:46 +0800

    Cleared template and implemented the base layout

--- kenji-docs/astro.config.mjs
+++ kenji-docs/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
-import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), solidJs()]
 });
