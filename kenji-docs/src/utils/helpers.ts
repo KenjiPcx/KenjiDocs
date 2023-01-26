cf088f3aa717a457f55fc6ea89d14b26da951b5a
Kenji Phang <ken.pcx@outlook.com>
2023-01-26 13:41:19 +0800

    Implemented all blogs page

---
+++ kenji-docs/src/utils/helpers.ts
@@ -0,0 +1,9 @@
+const formatDate = (date: Date) => {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+};
+
+export { formatDate };
