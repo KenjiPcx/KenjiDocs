c07a8ccbffcd99c2e43ee284182f4949f9b8e889
Kenji Phang <ken.pcx@outlook.com>
2023-01-27 11:44:06 +0800

    Added a placeholder for projects

--- kenji-docs/src/content/config.ts
+++ kenji-docs/src/content/config.ts
@@ -16,6 +16,29 @@ const blog = defineCollection({
   }),
 });
 
+const project = defineCollection({
+  schema: z.object({
+    isDraft: z.boolean(),
+    title: z.string(),
+    excerpt: z.string(),
+    tags: z.array(z.string()),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    category: z.enum([
+      "Development",
+      "Frontend Dev",
+      "Backend Dev",
+      "Fullstack Dev",
+      "Blockchain Dev",
+      "AI & ML",
+      "Mixed Reality",
+    ]),
+    publishDate: z.string().transform((str) => new Date(str)),
+  }),
+});
 export const collections = {
   blog,
+  project,
 };
---
+++ kenji-docs/src/content/project/example-project.md
@@ -0,0 +1,13 @@
+---
+isDraft: false
+title: "Example project"
+excerpt: "An example project"
+category: "Development"
+tags: ["test"]
+image: { src: "/assets/init-kenjidocs.png", alt: "Kenji + Docs = heart" }
+publishDate: "Jan 26, 2023"
+---
+
+## Hello World
+
+Hi there
